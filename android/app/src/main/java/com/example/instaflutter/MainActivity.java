82f533177fa63e8e8fb1535a0ae2b44788aef06c
Emily Fortuna <efortuna@google.com>
2018-09-05 16:06:04 -0700

    ERMAGERD IT WORRRRKS

--- android/app/src/main/java/com/example/instaflutter/MainActivity.java
+++ android/app/src/main/java/com/example/instaflutter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.instaflutter;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import net.sourceforge.simcpux.wxapi.WXEntryActivity;
 
 public class MainActivity extends FlutterActivity {
   @Override
